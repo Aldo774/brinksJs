1864268e61a3fe10a8e4f17b319177a0fcd3b637
Aldo774 <soares_aldo@hotmail.com>
2017-04-16 01:51:27 -0300

    [IMP][REF] Implementação de efeito para box seletor de mangas

--- js/script/script.js
+++ js/script/script.js
@@ -1,3 +1,6 @@
 $('#manga_container > div span:nth-child(3)').click(function(e){
     $('#manga_container').toggleClass('fullscreen'); 
 });
+$('#loadFile a, aside > ul li:nth-child(3)').click(function(e){
+    $('#loadFile').toggleClass('container_focus'); 
+});
